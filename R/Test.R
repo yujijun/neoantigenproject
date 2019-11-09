f3f11bb4350dc6f49def58a7c3b2524b254fad68
于吉军 <yujijun@yujijun.local>
2019-11-08 23:50:36 -0500

    Add original file for neoantigen project

---
+++ R/Test.R
@@ -0,0 +1,16 @@
+# --------------
+# Date:  2019-11-08 23:27:54 
+# Author:JijunYu
+# Email: jijunyu140@gmail.com
+# --------------
+# About project: This is a test function.
+# 
+#' This is a function for add 
+#' @param x is first parameter 
+#' @param y is second parameter
+#' @return x+y
+#' @author JijunYu
+#' @export
+add <- function(x,y){
+  return(x+y)
+}
